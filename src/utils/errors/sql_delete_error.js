124a814ea0d23a756a59bf3f008e5045f82ee979
jeff ritenburg <jeff@ns804.com>
2024-01-07 13:05:02 -0500

    forgot to include delete error.

--- src/utils/errors/sql_delete_error.js
+++ src/utils/errors/sql_delete_error.js
@@ -1,8 +1,8 @@
 const SQLError = require("./sql_error");
 
 class SQLDeleteError extends SQLError {
-    constructor(table, message, data) {
-        super("Delete failed on table '" + table + "': " + message + ".");
+    constructor(table, message, data, error) {
+        super("Delete failed on table '" + table + "': " + message + ".", error);
         this.name = "SQLDeleteError";
         this.data = data || {};
         return this;
